503c8db82e53173e13b7fb00fb854421eae1c23e
jack-payne <112025267+jack-payne@users.noreply.github.com>
2023-10-02 14:25:27 -0700

    Add files via upload

--- dbSetup.java
+++ dbSetup.java
@@ -1,5 +1,5 @@
 public final class dbSetup  {
   //TODO: Update user and pswd
-  public static final String user = "csce315950_Xuser";
+  public static final String user = "csce315_950_jackpayne";
   public static final String pswd = "password";
 }
